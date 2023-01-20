8edf26498a9bdaebd53396e8791f43050430552a
Keisha Josephs <kmjosephs@users.noreply.github.com>
2023-01-19 23:27:19 -0600

    coin info screen and team list item

--- app/src/main/java/com/example/coindex/MainActivity.kt
+++ app/src/main/java/com/example/coindex/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.coindex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.example.coindex.presentation.Screen
+import com.example.coindex.presentation.ui.theme.CoinDexTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            CoinDexTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.CoinListScreen.route
+                    ) {
+
+                } // end Navhost
+
+            }
+        }
     }
 }
--- app/src/main/java/com/example/coindex/presentation/coin_info/components/TeamListItem.kt
+++ app/src/main/java/com/example/coindex/presentation/coin_info/components/TeamListItem.kt
@@ -1,10 +1,37 @@
 package com.example.coindex.presentation.coin_info.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import com.example.coindex.data.remote.dto.TeamMember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.unit.dp
+
 
 @Composable
 fun TeamListItem(
-
+    teamMember: TeamMember,
+    modifier : Modifier = Modifier
 ){
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center
+    ){
+        Text (
+            text = teamMember.name,
+            style = MaterialTheme.typography.h4
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(
+            text = teamMember.position,
+            style = MaterialTheme.typography.body2,
+            fontStyle = FontStyle.Italic
+        )
 
+    }
 }
