74cc96cf20922dede95e00761400ca07b5145ab4
Keisha Josephs <kmjosephs@users.noreply.github.com>
2023-01-19 20:53:17 -0600

    coinlist states and viewmodel

--- app/src/main/java/com/example/coindex/presentation/coin_list/CoinListState.kt
+++ app/src/main/java/com/example/coindex/presentation/coin_list/CoinListState.kt
@@ -1,5 +1,9 @@
 package com.example.coindex.presentation.coin_list
 
-data class CoinListState(
+import com.example.coindex.domain.model.Coin
 
+data class CoinListState (
+    val isLoading: Boolean = false,
+    val coins: List<Coin> = emptyList(),
+    val error: String = ""
 )
