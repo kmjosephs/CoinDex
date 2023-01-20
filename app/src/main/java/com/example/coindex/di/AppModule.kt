5a1de932766ce2f94275e86dfea54ffcc691c0f3
Keisha Josephs <kmjosephs@users.noreply.github.com>
2023-01-19 19:34:04 -0600

    appmodule basics

--- app/src/main/java/com/example/coindex/di/AppModule.kt
+++ app/src/main/java/com/example/coindex/di/AppModule.kt
@@ -1,4 +1,22 @@
 package com.example.coindex.di
 
-class AppModule {
+import com.example.coindex.common.Constants
+import com.example.coindex.data.remote.CoinApi
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module@InstallIn(SingletonComponent::class)
+@Singleton
+object AppModule {
+    fun provideCoinApi(): CoinApi {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CoinApi::class.java)
+    }
 }
